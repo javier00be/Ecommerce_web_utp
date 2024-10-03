44c396ae89a6613c3fa9ab7407442a67b0d28e85
SebastianCTA <cruztaico16@gmail.com>
2024-10-03 00:34:33 -0500

    xd

--- fronted/src/app/components/public/admin/producto/producto.component.ts
+++ fronted/src/app/components/public/admin/producto/producto.component.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-producto',
-  templateUrl: './producto.component.html',
-  styleUrl: './producto.component.css'
+  selector: 'app-productos',
+  templateUrl: './productos.component.html',
+  styleUrls: ['./productos.component.css']
 })
-export class ProductoComponent {
+export class ProductosComponent {
+  productos = [
+    { img: 'assets/img/product1.jpg', nombre: 'Casaca Regular Fit', precio: 135 },
+    { img: 'assets/img/product2.jpg', nombre: 'Casaca Regular Fit', precio: 139 },
+    { img: 'assets/img/product3.jpg', nombre: 'Casaca Regular Fit', precio: 135 },
+    { img: 'assets/img/product4.jpg', nombre: 'Casaca Regular Fit', precio: 135 }
+  ];
 
+  nuevoProducto = {
+    nombre: '',
+    precio: null,
+    marca: '',
+    genero: '',
+    talla: '',
+    descripcion: '',
+    img: ''
+  };
+
+  actualizarProducto(producto: any) {
+    // Lógica para actualizar el producto
+    console.log('Actualizar producto:', producto);
+  }
+
+  guardarProducto() {
+    // Lógica para guardar el nuevo producto
+    console.log('Guardar producto:', this.nuevoProducto);
+  }
+
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+    // Lógica para manejar la subida de archivos
+    console.log('Archivo seleccionado:', file);
+  }
 }
-
