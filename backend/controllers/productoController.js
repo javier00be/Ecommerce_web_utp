8109a01753c8f3d09b6466d2284f698f59f3ac91
SebastianCTA <131300234+SebastianCTA@users.noreply.github.com>
2024-10-10 17:42:23 -0500

    xd

--- backend/controllers/productoController.js
+++ backend/controllers/productoController.js
@@ -1,21 +1,16 @@
 'use strict';
 const ProductoModel = require('./Models/productoModel'); // Asegúrate de que la ruta sea correcta
 
-const bcrypt = require('bcrypt');
+//const bcrypt = require('bcrypt');
 
 // Función para insertar un nuevo usuario
 async function insertarProducto(req, res) {
+
     // Extraer los campos del cuerpo de la solicitud
     const { nombre, precio, marca, genero, talla, descripcion } = req.body;
 
     try {
-        // Validar si ya existe un usuario con el mismo correo
-        const usuarioExistente = await UsuarioModel.findOne({ correo: correo });
 
-        if (usuarioExistente) {
-            return res.status(400).json({ message: 'El usuario ya está registrado con el mismo correo electrónico' });
-        }
-        
         // Crear el objeto del usuario con los datos proporcionados y la contraseña hasheada
         const nuevoProducto = new ProductoModel({
             nombre,
@@ -26,13 +21,13 @@ async function insertarProducto(req, res) {
             descripcion
         });
 
-        // Guardar el nuevo usuario en la base de datos
-        const usuarioGuardado = await nuevoUsuario.save();
+        // Guardar el nuevo producto en la base de datos
+        const productoGuardado = await nuevoProducto.save();
 
-        // Enviar la respuesta con el usuario guardado
+        // Enviar la respuesta con el producto guardado
         return res.status(201).json({
-            message: 'Usuario registrado correctamente',
-            data: usuarioGuardado
+            message: 'Producto registrado correctamente',
+            data: productoGuardado
         });
     } catch (error) {
         // Manejar errores y enviar respuesta con error
@@ -44,4 +39,4 @@ async function insertarProducto(req, res) {
 }
 
 // Exportar la función para usarla en otros archivos
-module.exports = { insertarUsuario };
+module.exports = { insertarProducto };
